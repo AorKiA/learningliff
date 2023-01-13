5225f6b06c0dc49236a5407d8ca3939f0457ab23
aorkia <40560461+AorKiA@users.noreply.github.com>
2023-01-13 16:40:06 +0700

    add files

---
+++ routes/multiBot.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+
+    let channelAccessToken = ""
+    let msg = ""
+
+    switch (req.body.desctination){
+        case "":
+            msg = ""
+            channelAccessToken = ""
+            break
+        case "":
+            msg = ""
+            channelAccessToken = ""
+            break
+    }
+
+    console.log("msg: " + msg)
+    console.log("token: " + channelAccessToken)
+    console.log("req: " + req)
+
+    // reply(
+    //     channelAccessToken,
+    //     req.body.events[0].replyToken,
+    //     { type: "text", text: msg }
+    // )
+
+
+    res.send('<b> Posted to API </b>');
+    // return null
+
+});
+
+const reply = (channelAccessToken, replyToken, payload) => {
+    request.post({
+        uri: "https://api.line.me/v2/bot/message/reply",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${channelAccessToken}`
+        },
+        body: JSON.stringify({
+            replyToken: replyToken,
+            messages: [payload]
+        })
+    })
+}
+
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/a', function(req, res, next) {
+  res.send('respond with a resource aaa');
+});
+
+
 module.exports = router;
